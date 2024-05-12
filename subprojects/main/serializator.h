47da928260c9986e8164cc85a872ba5f55e4a08a
fireilya <117916135+fireilya@users.noreply.github.com>
2024-05-12 12:32:04 +0500

    serialize ready

--- subprojects/main/serializator.h
+++ subprojects/main/serializator.h
@@ -1,191 +1,196 @@
 #pragma once
+#include "qdebug.h"
 #ifndef SERIALIZATOR_H
 #define SERIALIZATOR_H
 #include <QString>
 #include <QByteArray>
 #include <tuple>
-
+#include <type_traits>
 namespace Serializator
 {
 namespace __service{
 
-/*!
- * \brief Выполняет двоичную сериализацию одного элемента
- * \details Элемент должен быть фундаментальным типом или POD структурой.
- * Сложные типы (классы, управляющие данными в куче, другие контейнеры и т.д) не поддерживаются
- * \param <element> - элемент для сериализации
- * \param <answer> - ранее созданный QByteArray, куда будет помещён результат сериализации
- */
-template <typename T>
-void binarySerializeElement(const T& element, QByteArray& answer)
-{
-    const quint8* buf = reinterpret_cast<const quint8*>(&element);
-    for (quint32 i = 0; i < sizeof(element); i++) answer.append(buf[i]);
-}
+    template <typename ... >
+    using to_void = void;
 
-/*!
- * \brief Выполняет двоичную сериализацию одного контейнера
- * \details Контейнер должен содержать фундаментальные языковые типы или POD структуры.
- * Контейнеры, содержащие сложные типы (классы, управляющие данными в куче, другие контейнеры и т.д)
- * не поддерживаются
- * \param <container> - контейнер для сериализации
- * \param <answer> - ранее созданный QByteArray, куда будет помещён результат сериализации
- */
+    template<typename T, typename = void>
+    struct is_container : public std::false_type {};
 
-template <typename T>
-void binarySerializeContainer(T& container, QByteArray& answer)
-{
-    quint32 containerSize = container.size();
-    quint32 containerElementSize = sizeof(typename T::value_type);
-    binarySerializeElement(containerSize, answer);
-    answer.append(containerElementSize);
-    for (auto& el : container) binarySerializeElement(el, answer);
-}
-
-/*!
- * \brief Выполняет двоичную десериализацию одиночного элемента
- * \details Элемент должен быть фундаментальным типом или POD структурой.
- * Сложные типы (классы, управляющие данными в куче, другие контейнеры и т.д) не поддерживаются
- * \tparam <T> - выходной элемент
- * \param <bytes> - набор байтов для десериализации, содержащий десериализуемый элемент
- * \param <byteIndex> - Индекс первого байта десериализуемого элемента
- * \return элемент типа T, полученный в результате десериализации
- */
-template <typename T>
-T binaryDeserializeElement(const QByteArray& bytes, quint32& byteIndex)
-{
-    quint8* buf = new quint8[sizeof(T)];
-    for (quint32 j = 0; j < sizeof(T); ++j, ++byteIndex) buf[j] = bytes[byteIndex];
-    quint32 answer = *(reinterpret_cast<quint32*>(buf));
-    delete[] buf;
-    return answer;
-}
-/*!
- * \brief Выполняет двоичную десериализацию одиночного элемента
- * \details Элемент должен быть фундаментальным типом или POD структурой.
- * Сложные типы (классы, управляющие данными в куче, другие контейнеры и т.д) не поддерживаются
- * \tparam <T> - выходной элемент
- * \param <bytes> - набор байтов для десериализации, содержащий десериализуемый элемент
- * \param <byteIndex> - Индекс первого байта десериализуемого элемента
- * \param <allocatedBuf> - Аллоцированный буфер размера sizeof(T), в котором можно работать.
- * По завершению функции память не освобождается
- * \return элемент типа T, полученный в результате десериализации
- */
-
-template <typename T>
-T binaryDeserializeElement(const QByteArray& bytes, quint32& byteIndex, quint8* allocatedBuf)
-{
-    for (quint32 j = 0; j < sizeof(T); ++j, ++byteIndex) allocatedBuf[j] = bytes[byteIndex];
-    T answer = *(reinterpret_cast<T*>(allocatedBuf));
-    return answer;
-}
-
-/*!
- * \brief Выполняет двоичную десериализацию одного контейнера
- * \details Контейнер должен содержать фундаментальные языковые типы или POD структуры.
- * Контейнеры, содержащие сложные типы (классы, управляющие данными в куче, другие контейнеры и т.д)
- * не поддерживаются
- * \tparam <T> - выходной котейнер
- * \param <bytes> - набор байтов для десериализации, содержащий QT контейнер
- * \param <byteIndex> - Индекс первого байта десериализуемого контейнера
- * \return контейнер T, полученный в результате десериализации
- */
-template <typename T>
-T binaryDeserializeContainer(const QByteArray& bytes, quint32& byteIndex)
-{
-    T answer;
-    quint32 containerSize = binaryDeserializeElement<quint32>(bytes, byteIndex);
-    quint32 elementSize = bytes[byteIndex++];
-
-    if (elementSize != sizeof(typename T::value_type))
+    template <typename T>
+    struct is_container<T,
+        to_void<
+            decltype(std::declval<T>().begin()),
+            decltype(std::declval<T>().end()),
+            typename T::value_type>> : public std::true_type {};
+    /*!
+     * \brief Выполняет двоичную сериализацию одного элемента
+     * \details Элемент должен быть фундаментальным типом или POD структурой.
+     * Сложные типы (классы, управляющие данными в куче, другие контейнеры и т.д) не поддерживаются
+     * \param <element> - элемент для сериализации
+     * \param <answer> - ранее созданный QByteArray, куда будет помещён результат сериализации
+     */
+    template <typename T>
+    void binarySerializeElement(
+        const T& element,
+        QByteArray& answer,
+        typename std::enable_if_t<!is_container<T>::value, void**> = nullptr)
     {
-        QString errorMessage("Can't serialize bytes to");
-        errorMessage.append(typeid(T).name());
-        throw std::invalid_argument(errorMessage.toStdString());
+        qInfo()<<"element";
+        const quint8* buf = reinterpret_cast<const quint8*>(&element);
+        for (quint32 i = 0; i < sizeof(element); i++) answer.append(buf[i]);
     }
-    quint8* buf = new quint8[sizeof(typename T::value_type)];
-    for (quint32 i = 0; i < containerSize; i++)
+
+    /*!
+     * \brief Выполняет двоичную сериализацию одного контейнера
+     * \details Контейнер должен содержать фундаментальные языковые типы или POD структуры.
+     * Контейнеры, содержащие сложные типы (классы, управляющие данными в куче, другие контейнеры и т.д)
+     * не поддерживаются
+     * \param <container> - контейнер для сериализации
+     * \param <answer> - ранее созданный QByteArray, куда будет помещён результат сериализации
+     */
+
+    template <typename T>
+    void binarySerializeElement(
+        const T& container,
+        QByteArray& answer,
+        typename std::enable_if_t<is_container<T>::value, void**> = nullptr)
     {
-        answer.append(binaryDeserializeElement<typename T::value_type>(bytes, byteIndex, buf));
+        qInfo()<<"container";
+        quint32 containerSize = container.size();
+        quint32 containerElementSize = sizeof(typename T::value_type);
+        binarySerializeElement(containerSize, answer);
+        answer.append(containerElementSize);
+        for (auto& el : container) binarySerializeElement(el, answer);
     }
-    delete[] buf;
-    return answer;
+
+    /*!
+     * \brief Выполняет двоичную десериализацию одиночного элемента
+     * \details Элемент должен быть фундаментальным типом или POD структурой.
+     * Сложные типы (классы, управляющие данными в куче, другие контейнеры и т.д) не поддерживаются
+     * \tparam <T> - выходной элемент
+     * \param <bytes> - набор байтов для десериализации, содержащий десериализуемый элемент
+     * \param <byteIndex> - Индекс первого байта десериализуемого элемента
+     * \return элемент типа T, полученный в результате десериализации
+     */
+    template <typename T>
+    T binaryDeserializeElement(
+        const QByteArray& bytes,
+        quint32& byteIndex,
+        std::enable_if<!is_container<T>::value, void**> = nullptr)
+    {
+        quint8* buf = new quint8[sizeof(T)];
+        for (quint32 j = 0; j < sizeof(T); ++j, ++byteIndex) buf[j] = bytes[byteIndex];
+        T answer = *(reinterpret_cast<T*>(buf));
+        delete[] buf;
+        return answer;
+    }
+    /*!
+     * \brief Выполняет двоичную десериализацию одиночного элемента
+     * \details Элемент должен быть фундаментальным типом или POD структурой.
+     * Сложные типы (классы, управляющие данными в куче, другие контейнеры и т.д) не поддерживаются
+     * \tparam <T> - выходной элемент
+     * \param <bytes> - набор байтов для десериализации, содержащий десериализуемый элемент
+     * \param <byteIndex> - Индекс первого байта десериализуемого элемента
+     * \param <allocatedBuf> - Аллоцированный буфер размера sizeof(T), в котором можно работать.
+     * По завершению функции память не освобождается
+     * \return элемент типа T, полученный в результате десериализации
+     */
+
+    template <typename T>
+    T binaryDeserializeElement(
+        const QByteArray& bytes,
+        quint32& byteIndex,
+        quint8* allocatedBuf)
+    {
+        for (quint32 j = 0; j < sizeof(T); ++j, ++byteIndex) allocatedBuf[j] = bytes[byteIndex];
+        T answer = *(reinterpret_cast<T*>(allocatedBuf));
+        return answer;
+    }
+
+    /*!
+     * \brief Выполняет двоичную десериализацию одного контейнера
+     * \details Контейнер должен содержать фундаментальные языковые типы или POD структуры.
+     * Контейнеры, содержащие сложные типы (классы, управляющие данными в куче, другие контейнеры и т.д)
+     * не поддерживаются
+     * \tparam <T> - выходной котейнер
+     * \param <bytes> - набор байтов для десериализации, содержащий QT контейнер
+     * \param <byteIndex> - Индекс первого байта десериализуемого контейнера
+     * \return контейнер T, полученный в результате десериализации
+     */
+    template <typename T>
+    T binaryDeserializeElement(
+        const QByteArray& bytes,
+        quint32& byteIndex,
+        std::enable_if<is_container<T>::value, void**> = nullptr)
+    {
+        T answer;
+        quint32 containerSize = binaryDeserializeElement<quint32>(bytes, byteIndex);
+        quint32 elementSize = bytes[byteIndex++];
+
+        if (elementSize != sizeof(typename T::value_type))
+        {
+            QString errorMessage("Can't serialize bytes to");
+            errorMessage.append(typeid(T).name());
+            throw std::invalid_argument(errorMessage.toStdString());
+        }
+        quint8* buf = new quint8[sizeof(typename T::value_type)];
+        for (quint32 i = 0; i < containerSize; i++)
+        {
+            answer.append(binaryDeserializeElement<typename T::value_type>(bytes, byteIndex, buf));
+        }
+        delete[] buf;
+        return answer;
+    }
+
+    /*!
+     * \brief запрашивает двоичную десериализацию каждого контейнера в отдельности в том порядке,
+     * в котором они идут в выходном кортеже и добавляет результат в него
+     * \details Контейнеры должны содержать фундаментальные языковые типы или POD структуры.
+     * Контейнеры, содержащие сложные типы (классы, управляющие данными в куче, другие контейнеры и т.д)
+     * не поддерживаются
+     * \param <tuple> - выходной кортеж
+     * \param <bytes> - набор байтов для десериализации, содержащий QT контейнеры
+     * \param <byteIndex> - Индекс первого байта десериализуемого контейнера
+     */
+
+    template<
+        quint32 Lim,
+        quint32 I = 0,
+        typename Tuple,
+        typename Element = std::tuple_element_t <I, Tuple>>
+
+    void binaryDeserializeElementsProcess(Tuple& tuple, const QByteArray& bytes, quint32& byteIndex)
+    {
+        std::get<I>(tuple) = binaryDeserializeElement<Element>(bytes, byteIndex);
+        if constexpr (I + 1 != Lim)
+            binaryDeserializeElementsProcess<Lim, I + 1>(tuple, bytes, byteIndex);
+    }
 }
 
-/*!
- * \brief запрашивает двоичную десериализацию каждого контейнера в отдельности в том порядке,
- * в котором они идут в выходном кортеже и добавляет результат в него
- * \details Контейнеры должны содержать фундаментальные языковые типы или POD структуры.
- * Контейнеры, содержащие сложные типы (классы, управляющие данными в куче, другие контейнеры и т.д)
- * не поддерживаются
- * \param <tuple> - выходной кортеж
- * \param <bytes> - набор байтов для десериализации, содержащий QT контейнеры
- * \param <byteIndex> - Индекс первого байта десериализуемого контейнера
- */
-
-template<
-    quint32 Lim,
-    quint32 I = 0,
-    typename Tuple,
-    typename Container = std::tuple_element_t <I, Tuple>>
-
-void binaryDeserializeContainersProcess(Tuple& tuple, const QByteArray& bytes, quint32& byteIndex)
-{
-    std::get<I>(tuple) = binaryDeserializeContainer<Container>(bytes, byteIndex);
-    if constexpr (I + 1 != Lim)
-        binaryDeserializeContainersProcess<Lim, I + 1>(tuple, bytes, byteIndex);
-}
-}
-
-/*!
- * \brief Выполняет двоичную сериализацию N QT-контэйнеров в QByteArray, где N>0.
- * \details Контейнеры должны содержать фундаментальные языковые типы или POD структуры.
- * Контейнеры, содержащие сложные типы (классы, управляющие данными в куче, другие контейнеры и т.д)
- * не поддерживаются
- * \details Контейнеры будут сериализованы в том порядке, в котором были переданы
- * \param <container> - первый контейнер для сериализации
- * \param <otherContainers> - пакет добавочных контейнеров для сериализации (может быть пустым)
- * \return QByteArray, содержащий сериализованный контейнер
- */
-template<typename CurrentContainer, typename ... OtherContainers>
-QByteArray binarySerializeContainers(const CurrentContainer& container, const OtherContainers& ... otherContainers)
+template <typename Element, typename ... OtherElements>
+QByteArray binarySerialize(const Element& el, const OtherElements& ... elements)
 {
     QByteArray answer;
-    __service::binarySerializeContainer(container, answer);
-    if constexpr (sizeof...(otherContainers))
+    qInfo()<<__service::is_container<Element>::value;
+    __service::binarySerializeElement(el, answer);
+    if constexpr (sizeof...(OtherElements))
     {
-        QByteArray additional_bytes = binarySerializeContainers(otherContainers...);
-        answer.append(additional_bytes);
+        answer.append(binarySerialize(elements...));
     }
     return answer;
 }
 
-/*!
- * \brief Выполняет двоичную десериализацию набора байтов (QByteArray) в N QT-контейнеров, где N>0.
- * \details Набор байтов должен содержать в себе сериализованные контейнеры, содержащие
- * фундаментальные языковые типы или POD структуры.
- * Контейнеры, содержащие сложные типы (классы, управляющие данными в куче, другие контейнеры и т.д)
- * не поддерживаются
- * \tparam <Types...> - Пакет выходных контейнеров, которые должны получиться в ходе десериализации.
- *  Для корректной десериализации контейнеры в пакете должны располагаться в том же порядке,
- *  в котором они были сериализованы
- * \param <bytes> - набор байтов, содержащий сериализованные контейнеры
- * \return std::tuple<Types...>, содержащий запрошенные в пакете Types... контейнеры в том порядке,
- * в котором они были переданы в пакет
- */
-template<typename ... Types>
-std::tuple<Types...> binaryDeserializeContainers(const QByteArray& bytes)
+template <typename ... Types>
+std::tuple<Types...> binaryDeserialize(const QByteArray& bytes)
 {
     std::tuple<Types...> answer;
-    if constexpr (sizeof...(Types))
-    {
-        quint32 byteIndex = 0;
-
-        __service::binaryDeserializeContainersProcess<sizeof...(Types)>(answer, bytes, byteIndex);
-    }
+    __service::binaryDeserializeElementsProcess<sizeof...(Types)>(answer, bytes, 0);
     return answer;
 }
+
 };
+
+
 
 #endif // !SERIALIZATOR_H
 
